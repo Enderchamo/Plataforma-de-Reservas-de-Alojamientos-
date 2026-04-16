50944763cc090384d4a9e9345d6d9d45ab180147
Vic <gandergoku@gmail.com>
2026-04-16 19:14:27 -0400

    Arreglando el desastre de los controladores

--- PlataformaReservas.Aplicacion/Interfaces/IPhotoService.cs
+++ PlataformaReservas.Aplicacion/Interfaces/IPhotoService.cs
@@ -1,14 +1,8 @@
-using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Http; // Importante también aquí
 
 namespace PlataformaReservas.Aplicacion.Interfaces;
 
 public interface IPhotoService
 {
-    // Recibe el archivo, lo guarda y devuelve la ruta relativa .
     Task<string> GuardarFotoPropiedadAsync(IFormFile archivo);
-    
-    //para borrar la foto si se elimina la propiedad
-    void EliminarFotoPropiedad(string rutaRelativa);
 }
